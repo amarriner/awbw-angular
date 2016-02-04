7ff7ec750572e44797fb2f09db8109f8a2989145
Aaron Marriner <marrinea@strose.edu>
2016-02-04 14:05:04 -0500

    Starting over...

--- app/js/components/version/version.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('advanceWarsByWeb.version', [
-  'advanceWarsByWeb.version.interpolate-filter',
-  'advanceWarsByWeb.version.version-directive'
-])
-
-.value('version', '0.1');
