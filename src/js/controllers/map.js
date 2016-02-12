66fe8debeabb8d0fbf164946b3041751ed586c26
Aaron Marriner <amarriner@amarriner.com>
2016-02-12 12:00:15 -0500

    Refactored Data service so that it could be more easily used without dealing with callback hell

--- src/js/controllers/map.js
+++ src/js/controllers/map.js
@@ -13,66 +13,18 @@
     .controller('MapCtrl', ['$scope', 'Map', 'Data',
         function($scope, Map, Data) {
             
-            /*
-            $scope.getCountries = function(err, callback) {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
+            Data.getAll().then(function(response) {
                 
-                Data.getCountryData().then(function(response) {
-                    console.log('Found country data');
-                    $scope.countries = response.data;
-                    callback(null);
-                }).catch(function(response) {
-                    callback('Error getting country data'); 
+                $scope.menuData = response.menuData;
+                $scope.units = response.unitData;
+                $scope.countries = response.countryData;
+                $scope.terrain = response.terrainData;
+                
+                Map.get('56ba2e146d4875a31f1d66c6').then(function(response) {
+                    $scope.map = response; 
                 });
-            }
-            
-            $scope.getTerrain = function(callback) {
-                Data.getTerrainData().then(function(response) {
-                    console.log('Found terrain data');
-                    $scope.terrain = response.data;
-                    callback(null);
-                }).catch(function(response) {
-                    callback('Error getting terrain data'); 
-                });                                           
-            }
-
-            
-            $scope.getTerrain($scope.getCountryData);
-            */                
-            
-            Data.getMenuData().then(function(response) {
-                $scope.menuData = response;
+                
             });
-            
-            Data.getUnitData().then(function(response) {
-                
-                $scope.units = response.data;
-                
-                Data.getCountryData().then(function(response) {
-                
-                    $scope.countries = response.data;
-            
-                    Data.getTerrainData().then(function(response) {
-               
-                        $scope.terrain = response.data;
-                
-                        Map.get('56ba2e146d4875a31f1d66c6').then(function(response) {
-                
-                            $scope.map = response.data;
-                
-                            }).catch(function(response) {
-                            });
-                
-                        }).catch(function(response){
-                
-                    });
-            
-                }).catch(function(response) { });
-                
-            }).catch(function(response) { });
             
             $scope.range = function(min, max, step) {
                 step = step || 1;
@@ -81,19 +33,19 @@
                     input.push(i);
                 }
                 return input;
-            }
+            };
             
             $scope.getTerrainClass = function(i) {
                 return $scope.getCountry(i) + $scope.getTerrainName(i);
-            }
+            };
             
             $scope.getCountry = function(i) {
                 return ($scope.map.tiles[i].country || '');
-            }
+            };
             
             $scope.getTerrainName = function (i) {
                 return $scope.terrain[$scope.map.tiles[i].terrain].name.toLowerCase();
-            }
+            };
 
         }   
     ]);
